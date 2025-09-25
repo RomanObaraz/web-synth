RomanObaraz/web-synth

commit 1d2ba0215ca7825ba57a8fe74eabf98a600a611c

Toggleable styling

src/components/toggleable.jsx
@@ -1,6 +1,7 @@
-import { Card, CardContent, Switch, Typography } from "@mui/material";
+import { Card, CardContent, Checkbox, Typography } from "@mui/material";
 import { useState } from "react";
 import { useSynth } from "../hooks/useSynth";
+import { CheckBoxOutlineBlank, SquareRounded } from "@mui/icons-material";
 
 export const Toggleable = ({ moduleId, label, children }) => {
     const [enabled, setEnabled] = useState(true);
@@ -18,7 +19,12 @@ export const Toggleable = ({ moduleId, label, children }) => {
                     <Typography>{label}</Typography>
                 </div>
                 <div className="absolute right-0">
-                    <Switch checked={enabled} onChange={(e) => handleToggle(e.target.checked)} />
+                    <Checkbox
+                        checked={enabled}
+                        icon={<CheckBoxOutlineBlank />}
+                        checkedIcon={<SquareRounded />}
+                        onChange={() => handleToggle((prevEnabled) => !prevEnabled)}
+                    />
                 </div>
             </div>
             <CardContent

commit c76e5f5486ce55b5ba4313f579914cb0dbe319d1

fix Toggleable behaviour broken in commit 1d2ba0215ca7825ba57a8fe74eabf98a600a611c

src/components/toggleable.jsx
@@ -23,7 +23,7 @@ export const Toggleable = ({ moduleId, label, children }) => {
                         checked={enabled}
                         icon={<CheckBoxOutlineBlank />}
                         checkedIcon={<SquareRounded />}
-                        onChange={() => handleToggle((prevEnabled) => !prevEnabled)}
+                        onChange={(e) => handleToggle(e.target.checked)}
                     />
                 </div>
             </div>