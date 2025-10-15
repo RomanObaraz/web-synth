c8d6c8739d9c12b2b18549794963188d87ece6f3
Roman Obaraz <lexium729@gmail.com>
2025-10-15 14:41:51 +0300

    fix import typo

--- src/hooks/useSynth.js
+++ src/hooks/useSynth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { SynthContext } from "../SynthProvider/synthContext";
+import { SynthContext } from "../synthProvider/synthContext";
 
 export function useSynth() {
     return useContext(SynthContext);
